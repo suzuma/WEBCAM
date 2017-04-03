8888e9e0dee0c875ceb0c14c67d9e86a69df781d
SuZuMa <SuZuMa@SUZUMA-PC>
2017-04-03 10:15:39 -0700

    se agrega el codigo para mostrar la imagen en el index

--- DEMOLO_DATOS/CONSULTA/Controllers/HomeController.cs
+++ DEMOLO_DATOS/CONSULTA/Controllers/HomeController.cs
@@ -19,14 +19,6 @@ namespace CONSULTA.Controllers
             ViewBag.Datos = EmpleadoManager.getListado();
             return View();
         }
-        //http://www.prideparrot.com/blog/archive/2012/8/uploading_and_returning_files
-        public ActionResult GenerarImagen(int Id) {
-            Empleado empleado = EmpleadoManager.getByid(Id);
-            var bImagen = ToolImagen.Base64StringToBitmap(empleado.sImagen);
-            byte[] byteArray = ToolImagen.ImageToByte(bImagen); 
-
-            
-            return File(byteArray, "image/jpeg");
-        }
+      
     }
 }
